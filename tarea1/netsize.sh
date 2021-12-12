7293fc098692a84ad7d187eef3c06622a3ac59b6
Anbrebio30 <angel.ona@est.ikiam.edu.ec>
2021-12-11 22:42:40 -0500

    version 2

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -1,2 +1,5 @@
-# primer deber 
+#ejercicio 1.10.3 Plant-Pollinator Networks, página 5
+#Primero 
+wc -l ../../Saavedra2013/n3.txt > netsize.txt
+head -n1 ../../Saavedra2013/n3.txt | grep -o " " | wc -l >> netsize.txt
 
